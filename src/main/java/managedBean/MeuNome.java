dec81acfccd0f670956026f9bf430b7e810a5324
fabiano <fabiomsrs@gmail.com>
2017-06-14 07:06:55 -0300

    fix bug 2

--- src/main/java/managedBean/MeuNome.java
+++ src/main/java/managedBean/MeuNome.java
@@ -1,5 +1,19 @@
 package managedBean;
 
-public class MeuNome {
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
+@ManagedBean
+@ViewScoped
+public class MeuNome {
+	private String meuNome;
+
+	public String getMeuNome() {
+		return meuNome;
+	}
+
+	public void setMeuNome(String meuNome) {
+		this.meuNome = meuNome;
+	}
+	
 }
